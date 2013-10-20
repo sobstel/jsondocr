0bdc463ef0d31f575bbb1a7e8d15f149ce58f854
Przemek Sobstel <przemek@sobstel.org>
2013-10-20 18:56:36 +0200

    minor builders/behaviors restructuring

--- lib/jsondocr/builders/behaviors/validation.rb
+++ lib/jsondocr/builders/behaviors/validation.rb
@@ -1,16 +1,14 @@
-module JSONdocr::Builders
-  module Behaviors
-    module Validation
-      def self.included(base)
-        base.dsl_attr :validations
-      end
+module JSONdocr::Builders::Behaviors
+  module Validation
+    def self.included(base)
+      base.dsl_attr :validations
+    end
 
-      # Adds validation rule
-      def validates(*attrs)
-        @validations ||= []
-        @validations << attrs
-        attrs
-      end
+    # Adds validation rule
+    def validates(*attrs)
+      @validations ||= []
+      @validations << attrs
+      attrs
     end
   end
 end
